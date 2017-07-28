99501715525f2907a88e1324811f9a6ee0d8741a
no_fate <den.uln73@gmail.com>
2017-07-28 21:56:27 +0400

    First commit. First hometask - done.

---
+++ SimbirSoftAndroidHomeWork/app/src/main/java/com/no_fate/simbirsoftandroidhomework/MainActivity.java
@@ -0,0 +1,13 @@
+package com.no_fate.simbirsoftandroidhomework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
